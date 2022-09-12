9979676c1ffc0f0e4b5c51df59617b8e283830ab
Dohun Lee <nestour95@gmail.com>
2022-09-12 17:45:22 +0900

    feat: Security Config에 작성한 Filter,Handler 및 Cors옵션 추가
    Filter,Handler를 적용하기위해 Security Config에 등록하였습니다.
    
    더불어 프론트 단과 소통하기위해 필요한 Cors 오션 설정을 추가했습니다.

--- src/main/java/chocoteamteam/togather/config/SecurityConfig.java
+++ src/main/java/chocoteamteam/togather/config/SecurityConfig.java
@@ -1,15 +1,29 @@
 package chocoteamteam.togather.config;
 
+import chocoteamteam.togather.component.security.JwtAccessDeniedHandler;
+import chocoteamteam.togather.component.security.JwtAuthenticationFilter;
+import chocoteamteam.togather.component.security.JwtEntryPoint;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final JwtEntryPoint jwtEntryPoint;
+	private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,7 +32,8 @@ public class SecurityConfig {
 
 		http.headers().frameOptions().disable();
 
-		http.cors().disable()
+		http.cors().configurationSource(corsConfigurationSource())
+			.and()
 			.httpBasic().disable()
 			.csrf().disable()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -26,7 +41,28 @@ public class SecurityConfig {
 		http.formLogin().disable();
 		http.logout().disable();
 
+		http.exceptionHandling()
+			.authenticationEntryPoint(jwtEntryPoint)
+			.accessDeniedHandler(jwtAccessDeniedHandler);
+
+		http.addFilterBefore(jwtAuthenticationFilter,
+			UsernamePasswordAuthenticationFilter.class);
+
 		return http.build();
+	}
+
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOriginPattern("*");
+		configuration.addAllowedMethod("*");
+		configuration.addAllowedHeader("*");
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 }
