affa63b2aef72e19cc6e435af85c4770b40ad2da
moo-cow <devjy0309@gmail.com>
2022-09-17 16:55:18 +0900

    feat: 프로젝트 검색 및 조회 기능
    - 기존 controller, service, repository를 사용하기 위해 interface로 구현
    - querydsl을 이용한 조회 쿼리 개발

--- src/main/java/chocoteamteam/togather/repository/ProjectRepository.java
+++ src/main/java/chocoteamteam/togather/repository/ProjectRepository.java
@@ -3,5 +3,5 @@ package chocoteamteam.togather.repository;
 import chocoteamteam.togather.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+public interface ProjectRepository extends JpaRepository<Project, Long>, QueryDslProjectRepository {
 }
