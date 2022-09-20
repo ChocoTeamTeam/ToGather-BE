89e367cf1f1a20862ee712413365c5b6750050f4
SeongHyeon <malslapq@gmail.com>
2022-09-20 20:13:20 +0900

    refactor: 회원 정보 수정 service 로직 메서드로 나누어 리팩토링

--- src/main/java/chocoteamteam/togather/service/MemberService.java
+++ src/main/java/chocoteamteam/togather/service/MemberService.java
@@ -80,38 +80,56 @@ public class MemberService {
         /*  닉네임으로 회원을 가져와 회원이 존재할 경우
             해당 회원 Id과 현재 수정을 요청한 회원 Id를 비교해 다를 경우 예외 발생
          */
-        Optional<Member> byNickname = memberRepository.findByNickname(request.getNickname());
-        if (byNickname.isPresent() && !Objects.equals(byNickname.get().getId(), memberId)) {
-            throw new MemberException(ErrorCode.EXIST_TRUE_MEMBER_NICKNAME);
-        }
+        checkMemberIdAndNickname(request.getNickname(), memberId);
 
         //  회원과 회원이 가지고 있는 기술 스택 한 번에 가져옴 회원이 존재하지 않을 경우 예외 발생
         Member member = memberRepository.findWithMemberTechStackById(memberId)
             .orElseThrow(() -> new MemberException(ErrorCode.NOT_FOUND_MEMBER));
+
+        // 회원 정보 수정
         member.modifyNicknameAndProfileImage(request.getNickname(), request.getProfileImage());
 
         // 삭제할 MemberTechStackId와 저장할 TechStackId를 구분
         List<MemberTechStack> memberTechStacks = member.getMemberTechStacks();
         Set<Long> requestTechStackIds = new HashSet<>(request.getTechStackDtos());
         Set<Long> deleteMemberTechStackIds = new HashSet<>();
-
-        for (MemberTechStack memberTechStack : memberTechStacks) {
-            Long curMemberTechStackId = memberTechStack.getTechStack().getId();
-            if (requestTechStackIds.contains(curMemberTechStackId)) {
-                requestTechStackIds.remove(curMemberTechStackId);
-                continue;
-            }
-            deleteMemberTechStackIds.add(curMemberTechStackId);
-        }
+        initMemberTechStackDeleteAndInsertIds(
+            memberTechStacks,
+            requestTechStackIds,
+            deleteMemberTechStackIds
+        );
 
         // 삭제할 MemberTechStackId List가 비어있지 않다면 삭제
-        if (!deleteMemberTechStackIds.isEmpty()) {
-            memberTechStackRepository.deleteAllByIdInQuery(deleteMemberTechStackIds);
-        }
+        deleteByMemberTechStackIds(deleteMemberTechStackIds);
 
         // 저장할 TechStackId List가 비어있지 않다면 저장
-        if (!requestTechStackIds.isEmpty()) {
-            List<TechStack> techStacks = techStackRepository.findAllById(requestTechStackIds);
+        saveByMemberTechStack(member, requestTechStackIds);
+    }
+
+    private void initMemberTechStackDeleteAndInsertIds(List<MemberTechStack> memberTechStacks,
+        Set<Long> insertIds,
+        Set<Long> deleteIds) {
+        for (MemberTechStack memberTechStack : memberTechStacks) {
+            Long curMemberTechStackId = memberTechStack.getTechStack().getId();
+            if (insertIds.contains(curMemberTechStackId)) {
+                insertIds.remove(curMemberTechStackId);
+                continue;
+            }
+            deleteIds.add(curMemberTechStackId);
+        }
+    }
+
+    private void checkMemberIdAndNickname(String nickname, Long memberId) {
+        Optional<Member> byNickname = memberRepository.findByNickname(nickname);
+        if (byNickname.isPresent() && !Objects.equals(byNickname.get().getId(), memberId)) {
+            throw new MemberException(ErrorCode.EXIST_TRUE_MEMBER_NICKNAME);
+        }
+
+    }
+
+    private void saveByMemberTechStack(Member member, Set<Long> techStackIds) {
+        if (!techStackIds.isEmpty()) {
+            List<TechStack> techStacks = techStackRepository.findAllById(techStackIds);
             for (TechStack techStack : techStacks) {
                 memberTechStackRepository.save(MemberTechStack.builder()
                     .member(member)
@@ -119,7 +137,12 @@ public class MemberService {
                     .build());
             }
         }
+    }
 
+    private void deleteByMemberTechStackIds(Set<Long> ids) {
+        if (!ids.isEmpty()) {
+            memberTechStackRepository.deleteAllByIdInQuery(ids);
+        }
     }
 
 }
