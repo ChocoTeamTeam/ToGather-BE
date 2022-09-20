936134aa6a9802ec57476503cf641718055ea893
SeongHyeon <malslapq@gmail.com>
2022-09-20 19:05:00 +0900

    feat: Member Repository에 MemberTechStack 같이 가져오는 메서드 추가

--- src/main/java/chocoteamteam/togather/repository/MemberRepository.java
+++ src/main/java/chocoteamteam/togather/repository/MemberRepository.java
@@ -2,11 +2,18 @@ package chocoteamteam.togather.repository;
 
 import chocoteamteam.togather.entity.Member;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByNickname(String nickname);
+
+    @EntityGraph(attributePaths = {"memberTechStacks"}, type = EntityGraphType.LOAD)
+    Optional<Member> findWithMemberTechStackById(Long id);
 
     boolean existsByNickname(String nickname);
 
