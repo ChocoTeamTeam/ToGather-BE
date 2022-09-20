cca711a0b2373ec86c28c263e27d1fce1effa292
Dohun Lee <nestour95@gmail.com>
2022-09-20 13:27:24 +0900

    feat: 회원 탈퇴 API 추가
    회원 탈퇴처리는 회원 본인이거나 어드민만 가능하도록 권한처리 하였습니다.

--- src/main/java/chocoteamteam/togather/controller/MemberController.java
+++ src/main/java/chocoteamteam/togather/controller/MemberController.java
@@ -2,16 +2,21 @@ package chocoteamteam.togather.controller;
 
 import chocoteamteam.togather.dto.MemberDetailResponse;
 import chocoteamteam.togather.service.MemberService;
+import chocoteamteam.togather.type.MemberStatus;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Member", description = "회원 관련 API")
 @RequestMapping("/members")
 @RestController
 @RequiredArgsConstructor
@@ -28,4 +33,19 @@ public class MemberController {
         return ResponseEntity.ok(memberService.getDetail(memberId));
     }
 
+	@Operation(
+		summary = "회원 탈퇴", description = "유저 본인 혹은 어드민이 회원탈퇴를 진행할 수 있습니다.",
+		security = {@SecurityRequirement(name = "Authorization")},
+		tags = {"Member"}
+	)
+	@PreAuthorize("hasRole('USER') and principal.id == #memberId or hasRole('ADMIN')")
+	@PostMapping("/members/{memberId}/withdrawal")
+	public ResponseEntity withdrawal(@PathVariable("memberId") Long memberId) {
+
+		memberService.changeStatus(memberId, MemberStatus.WITHDRAWAL);
+
+		return ResponseEntity.ok().body("");
+	}
+
 }
+
