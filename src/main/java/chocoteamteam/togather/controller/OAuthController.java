26e2ac71f749b9d5a02af70f4a9bf39bf14e7681
Dohun Lee <nestour95@gmail.com>
2022-09-20 12:18:08 +0900

    feat: 로그아웃 API 작성 (#26)
    * feat: 로그아웃 API 작성
    
    로그아웃 시, refresh Token을 삭제합니다.
    로그인이 되어있어야 로그아웃이 가능합니다.
    
    * chore: OAuthController Swagger 추가 및 ResponseEntity로 반환하도록 수정
    
    Swagger가 미비했던 부분을 간단하게 정리하여 추가했습니다.
    더불어 ResponseEntity로 반환값을 한번 감싸도록 하였습니다.

--- src/main/java/chocoteamteam/togather/controller/OAuthController.java
+++ src/main/java/chocoteamteam/togather/controller/OAuthController.java
@@ -1,19 +1,35 @@
 package chocoteamteam.togather.controller;
 
+import chocoteamteam.togather.dto.LoginMember;
 import chocoteamteam.togather.dto.LoginResponse;
 import chocoteamteam.togather.dto.SignUpControllerDto;
 import chocoteamteam.togather.dto.SignUpServiceDto;
+import chocoteamteam.togather.dto.TokenMemberInfo;
+import chocoteamteam.togather.service.JwtService;
 import chocoteamteam.togather.service.OAuthService;
+import chocoteamteam.togather.type.MemberStatus;
+import chocoteamteam.togather.type.Role;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
+@Tag(name = "OAuth", description = "인증 관련 API")
 @RequestMapping("/oauth")
 @RestController
 @RequiredArgsConstructor
@@ -21,21 +37,42 @@ public class OAuthController {
 
     private final OAuthService oAuthService;
 
+    @Operation(
+        summary = "로그인", description = "Provider들이 제공하는 code를 RequestBody에 입력",
+        tags = {"OAuth"}
+    )
     @PostMapping("/login/{provider}")
-    public LoginResponse login(@RequestBody String code, @PathVariable String provider) {
-        return oAuthService.login(code, provider);
+    public ResponseEntity<LoginResponse> login(@RequestBody String code, @PathVariable String provider) {
+        return ResponseEntity.ok().body(oAuthService.login(code, provider));
     }
 
+    @Operation(
+        summary = "회원가입", description = "signUpToken 헤더로 회원가입",
+        tags = {"OAuth"}
+    )
     @PostMapping("/signup")
-    public SignUpControllerDto.Response signup(
+    public ResponseEntity<SignUpControllerDto.Response> signup(
         @RequestBody @Valid SignUpControllerDto.Request request,
         @NotEmpty @RequestHeader(value = "signUpToken") String signUpToken) {
-        return oAuthService.signUp(SignUpServiceDto.builder()
+        return ResponseEntity.ok().body(oAuthService.signUp(SignUpServiceDto.builder()
             .signUpToken(signUpToken)
             .nickname(request.getNickname())
             .profileImage(request.getProfileImage())
             .techStackDtoList(request.getTechStackDtos())
-            .build());
+            .build()));
+    }
+
+
+    @Operation(
+        summary = "로그아웃", description = "로그인 되어있는 유저나 어드민은 로그아웃 할 수 있습니다.",
+        security = {@SecurityRequirement(name = "Authorization")},
+        tags = {"OAuth"}
+    )
+    @Secured({"ROLE_USER","ROLE_ADMIN"})
+    @PostMapping("/logout")
+    public ResponseEntity logout(@ApiIgnore @AuthenticationPrincipal LoginMember loginMember) {
+        oAuthService.logout(loginMember.getId());
+        return ResponseEntity.ok().body("");
     }
 
 }
