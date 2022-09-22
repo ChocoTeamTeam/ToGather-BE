8c4e4fc965dfebfae2a194420d06f76d76f09647
Dohun Lee <nestour95@gmail.com>
2022-09-22 16:55:23 +0900

    feat: ChatRoom,ProjectMember Entity 생성
    ERD따라 작성했습니다.

---
+++ src/main/java/chocoteamteam/togather/entity/ProjectMember.java
@@ -0,0 +1,35 @@
+package chocoteamteam.togather.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class ProjectMember extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn
+	private Project project;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn
+	private Member member;
+
+}
