a30c2c0100bfa8eeaae7d643122ddbffcac30cd1
Dohun Lee <nestour95@gmail.com>
2022-09-22 21:16:36 +0900

    feat: 채팅방 리스트 조회 API 구현
    테스트는 입력값과 출력값 테스트 및 pathvariable과 pricipal 값이 제대로 들어가는지 테스트 했습니다.

--- src/main/java/chocoteamteam/togather/controller/ProjectChatRoomController.java
+++ src/main/java/chocoteamteam/togather/controller/ProjectChatRoomController.java
@@ -7,11 +7,13 @@ import chocoteamteam.togather.service.ProjectChatRoomService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,6 +44,21 @@ public class ProjectChatRoomController {
 		form.setMemberId(member.getId());
 
 		return ResponseEntity.ok().body(projectChatRoomService.createChatRoom(form));
+	}
+
+	@Operation(
+		summary = "채팅방 조회", description = "프로젝트 멤버만 조회가능",
+		security = {@SecurityRequirement(name = "Authorization")},
+		tags = {"Chat"}
+	)
+	@PreAuthorize("hasRole('USER')")
+	@GetMapping("/{projectId}/chats")
+	public ResponseEntity<List<ChatRoomDto>> getProjectChats(
+		@ApiIgnore @AuthenticationPrincipal LoginMember member,
+		@PathVariable long projectId) {
+
+		return ResponseEntity.ok()
+			.body(projectChatRoomService.getChatRooms(projectId, member.getId()));
 	}
 
 
--- src/test/java/chocoteamteam/togather/controller/ProjectChatRoomControllerTest.java
+++ src/test/java/chocoteamteam/togather/controller/ProjectChatRoomControllerTest.java
@@ -1,7 +1,11 @@
 package chocoteamteam.togather.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -14,8 +18,11 @@ import chocoteamteam.togather.service.JwtService;
 import chocoteamteam.togather.service.ProjectChatRoomService;
 import chocoteamteam.togather.testUtils.WithLoginMember;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -65,5 +72,38 @@ class ProjectChatRoomControllerTest {
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.roomId").value(dto.getRoomId()))
 			.andExpect(jsonPath("$.roomName").value(dto.getRoomName()));
+	}
+
+	@WithLoginMember
+	@DisplayName("채팅방 리스트 조회 API 성공")
+	@Test
+	void getProjectChats_success() throws Exception {
+		//given
+		ChatRoomDto dto = ChatRoomDto.builder()
+			.roomId(1L)
+			.roomName("test")
+			.build();
+
+		List<ChatRoomDto> chatRoomDtos = Arrays.asList(dto);
+
+		given(projectChatRoomService.getChatRooms(anyLong(), anyLong()))
+			.willReturn(chatRoomDtos);
+
+		ArgumentCaptor<Long> memberIdCaptor = ArgumentCaptor.forClass(Long.class);
+		ArgumentCaptor<Long> projectIdCaptor = ArgumentCaptor.forClass(Long.class);
+
+		//when
+		//then
+		mockMvc.perform(get("/projects/1/chats"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$[0].roomId").value(dto.getRoomId()))
+			.andExpect(jsonPath("$[0].roomName").value(dto.getRoomName()));
+
+		verify(projectChatRoomService)
+			.getChatRooms(projectIdCaptor.capture(), memberIdCaptor.capture());
+
+		assertThat(projectIdCaptor.getValue()).isEqualTo(1L);
+		assertThat(memberIdCaptor.getValue()).isEqualTo(1L);
 	}
 }
