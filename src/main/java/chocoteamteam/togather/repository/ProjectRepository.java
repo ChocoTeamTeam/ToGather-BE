7052be696500db3b086bf71bda88e12fffe05ff9
moo-cow <devjy0309@gmail.com>
2022-09-28 17:44:53 +0900

    feat: job 생성

--- src/main/java/chocoteamteam/togather/repository/ProjectRepository.java
+++ src/main/java/chocoteamteam/togather/repository/ProjectRepository.java
@@ -1,7 +1,15 @@
 package chocoteamteam.togather.repository;
 
 import chocoteamteam.togather.entity.Project;
+import chocoteamteam.togather.type.ProjectStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ProjectRepository extends JpaRepository<Project, Long>, QueryDslProjectRepository {
+
+    Page<Project> findAllByStatusAndDeadlineBefore(ProjectStatus status, LocalDate deadline, Pageable pageable);
 }
