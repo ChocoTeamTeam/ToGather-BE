8360c0e2fc4274be806f85a64dc0342a1946c45d
한꼬북 <malslapq@gmail.com>
2022-10-01 18:05:28 +0900

    feat: 팀 채팅 기능 구현 (#69)
    * feat: 웹소켓 설정 파일 및 build.gradle dependencies 추가
    
    * feat: 채팅 시, JWT 인증 기능 구현 (#54)
    
    * feat: 웹소켓 연결 시 , JWT 인증 구현 
    
    웹 소켓 연결 시, STOMP 헤더 중 Authorization을 파싱하여 인증하는 기능을 구현했습니다.
    
    * feat: WebSocket JWT 인증 오류 핸들링 추가
    
    STOMP로 통신 중에 TokenException이 발생하면 예외 처리를 하는 핸들러를 추가했습니다.
    
    * hot-fix: 의존성 주입 오류 수정
    
    의존성 주입을 빼먹어서 급하게 넣습니다.
    
    * feat: 팀 채팅 전송 기능 구현 (#55)
    
    * feat: TopicExchange, Binding Bean 추가, Custom Queue 제거
    
    * chore: import 빠진 부분 추가
    
    * feat: 채팅 전송 기능 구현
    
    * chore: 계정 root에서 admin으로 변경
    
    기본 계정인 guest 사용하지 않아 StompConfig에 설정 추가
    
    Co-authored-by: Dohun Lee <nestour95@gmail.com>

---
+++ src/main/java/chocoteamteam/togather/config/StompConfig.java
@@ -0,0 +1,47 @@
+package chocoteamteam.togather.config;
+
+import chocoteamteam.togather.component.stomp.ChatErrorHandler;
+import chocoteamteam.togather.component.stomp.StompJwtHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompJwtHandler stompJwtHandler;
+    private final ChatErrorHandler chatErrorHandler;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stomp/chat")
+            .setAllowedOriginPatterns("*")
+            .withSockJS();
+        registry.setErrorHandler(chatErrorHandler);
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setPathMatcher(new AntPathMatcher("."));
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue")
+            .setRelayHost("localhost")
+            .setRelayPort(61613)
+            .setClientLogin("admin")
+            .setClientPasscode("admin")
+            .setSystemLogin("admin")
+            .setSystemPasscode("admin");
+
+    }
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompJwtHandler);
+    }
+}
