03ede74eaaf6e3106fed0bd3e2278452440d872e
eongiin <eongiin@gmail.com>
2022-10-05 06:10:35 +0900

    chore: WeeklyDeadlineProjectJob 삭제
    사용자 맞춤 마감 임박 공고를 선정하는 방식을 바꿔서 기존에 있던 마감 임박 공고 저장 job은 삭제했습니다

--- src/main/java/chocoteamteam/togather/batch/domain/repository/WeeklyDeadlineProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package chocoteamteam.togather.batch.domain.repository;
-
-import chocoteamteam.togather.batch.domain.entity.WeeklyDeadlineProject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WeeklyDeadlineProjectRepository extends JpaRepository<WeeklyDeadlineProject, Long> {
-}
