eaab6a95c8fbc43d4739084cf6031ef4e5273a0a
SeongHyeon <malslapq@gmail.com>
2022-10-12 11:44:00 +0900

    chore: 이미지 컨트롤러 스웨거 추가

--- src/main/java/chocoteamteam/togather/controller/ImageController.java
+++ src/main/java/chocoteamteam/togather/controller/ImageController.java
@@ -1,6 +1,7 @@
 package chocoteamteam.togather.controller;
 
 import chocoteamteam.togather.service.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,10 @@ public class ImageController {
 
     private final ImageService imageService;
 
+    @Operation(
+        summary = "이미지 저장 api", description = "이미지를 업로드하고 해당 이미지로 접근할 수 있는 Url 응답",
+        tags = {"Image"}
+    )
     @PostMapping
     public ResponseEntity<String> upload(@RequestBody MultipartFile file) {
         return ResponseEntity.ok(imageService.upload(file));
